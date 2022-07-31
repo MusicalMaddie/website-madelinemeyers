535e25de0329fc291a347648342e3e79b9162325
Maddie Meyers <maddiemeyers@outlook.com>
2022-07-30 20:34:32 -0400

    further updates

--- Server Files/public_html/index.php
+++ Server Files/public_html/index.php
@@ -1,56 +1,55 @@
 <html>
+
  <head>
   <link href="maddiestyle.css" rel="stylesheet">
   <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,500,100' rel='stylesheet' type='text/css'>
-   <title>M.Meyers Projects & Info</title>
-   <link rel="maddieIcon" type="image/x-icon" href="favicon.ico">
+  <title>M.Meyers Projects & Info</title>
+  <link rel="maddieIcon" type="image/x-icon" href="favicon.ico">
  </head>
 
  <body>
   <nav id="navTop">
-    <a class="btn" href="ShowcasePage/examples.html" target="_blank" rel="noopener noreferrer">Project Showcases!</a>
-     <a class="btn" href="AdobeExamples/page.html" target="_blank" rel="noopener noreferrer">Adobe Examples!</a>
-     <a class="btn" href="ResumePage/page.html" target="_blank" rel="noopener noreferrer">My Full Resume</a>
-    </nav>
- <div class = "header">
+    <a class="btn" href="ShowcasePage/examples.html" target="_blank" rel="noopener noreferrer">Project Showcases</a>
+    <a class="btn" href="AdobeExamples/page.html" target="_blank" rel="noopener noreferrer">Adobe Examples</a>
+    <a class="btn" href="ResumePage/page.html" target="_blank" rel="noopener noreferrer">Resume PDF</a>
+  </nav>
+  <div class = "header">
     <div id="titleContainer">
-            <h1 id="mainTitle">Madeline Meyers</h1>
-            <div id="subTitleContainer">
-              <div>
-                <p id="subTitle">2022</p>
-              </div>
-              <div>
-                <p id="subTitle">Tech Elevator Graduate</p>
-              </div>
-            </div>
-            
-    </div>
+      <h1 id="mainTitle">Madeline Meyers</h1>
+        <div id="subTitleContainer">
+          <div>
+            <p id="subTitle">2022</p>
+          </div>
+          <div>
+            <p id="subTitle">Tech Elevator Graduate</p>
+          </div>
+        </div>       
+      </div>
     <div id="featured">
       <a class="btn" href="JumpingGamePage/JavaScriptJumpingGame/MaddieJumpingGame/MaddieJumpingGame/MaddieJumpingGameContents/index.html">Try out 'Maddie's JavaScript Jumping Game'!!</a>
     </div>
     <div class = "container" id="buttonList">  
-        <a class="btn" href="https://www.linkedin.com/in/maddiemeyers/" target="_blank" rel="noopener noreferrer">Madeline's LinkedIn</a>
-        <a class="btn" href="https://www.freecodecamp.org/musicalmaddie" target="_blank" rel="noopener noreferrer">Madeline's freeCodeCamp</a>
-        <a class="btn" href="https://github.com/MusicalMaddie" target="_blank" rel="noopener noreferrer">Madeline's GitHub</a>
-            <a class="btn" href="https://www.codecademy.com/profiles/MadelineMeyers" target="_blank" rel="noopener noreferrer">Madeline's codecademy</a>
-            <a class="btn" href="hhttps://www.youtube.com/channel/UCV4GYJJ1FNMYQt0ltjA_Jkw/featured" target="_blank" rel="noopener noreferrer">Madeline's Youtube</a>
+      <a class="btn" href="https://www.linkedin.com/in/maddiemeyers/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      <a class="btn" href="https://www.freecodecamp.org/musicalmaddie" target="_blank" rel="noopener noreferrer">FreeCodeCamp</a>
+      <a class="btn" href="https://github.com/MusicalMaddie" target="_blank" rel="noopener noreferrer">GitHub</a>
+      <a class="btn" href="https://www.codecademy.com/profiles/MadelineMeyers" target="_blank" rel="noopener noreferrer">CodeAcademy</a>
+      <a class="btn" href="https://www.youtube.com/channel/UCV4GYJJ1FNMYQt0ltjA_Jkw/featured" target="_blank" rel="noopener noreferrer">Youtube</a>
+      <a class="btn" href="https://app.codesignal.com/profile/musicalmaddie" target="_blank" rel="noopener noreferrer">CodeSignal</a>
+      <a class="btn" href="https://leetcode.com/maddiemeyers/" target="_blank" rel="noopener noreferrer">LeetCode</a>
+      <a class="btn" href="https://www.hackerrank.com/maddiemeyers" target="_blank" rel="noopener noreferrer">HackerRank</a>
     </div>
-
     <div class="main">
         <div class="container">
             <h2 id="descriptionTitle">A vocal, passionate, and resilient Junior Developer</h2>
             <p id="description">This website was created solely from the code, ground up, using empty files hosted on SiteGround. Clicking through Madeline's other projects, you'll find plenty of passions that demonstrate how Madeline is always ready to learn something new. Please contact her using the form provided, or directly at maddiemeyers@outlook.com</p>
-
     </div>
-
     <div class="jumbotron">
       <div class="container">
         <h2>Stay informed</h2>
-        <p>Want to know what Maddie is up to?</p>
+        <p id="followDescription">Want to know what Maddie is up to?</p>
         <a class="btn" href="#">Follow Maddie</a>
       </div>
     </div>
-
     <div class="footer">
       <div class="container">
         <p>&copy; Madeline Meyers 2022</p>
